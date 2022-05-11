b3f8a7f8e5cba4391bd6af33355cc1b500a820be
Nikhil <nsoni19012001@gmail.com>
2022-05-11 22:44:05 +0530

    Axios added

---
+++ src/api/unsplash.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'https://api.unsplash.com',
+  headers: {
+    Authorization: 'Client-ID Hx6oS1On6tUt98t1dogn9Rx9sZ-t_Fknu5S17szr9hY',
+  },
+});
+const searchImages = (term) => {};
